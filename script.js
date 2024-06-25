d2fa6c26bda9874900098a3fe2383aa07787a998
anibjee <anibjee@gmail.com>
2024-06-26 01:55:28 +0530

    Update script.js
    Correcting when clicking "BackSpace" when there is "Error"

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ function clearDisplay() {
   display.value = "";
 }
 function BackSpace() {
+  if (display.value === "Error") {
+    clearDisplay();
+  }
   display.value = display.value.slice(0, display.value.length - 1);
 }
 function calculate() {
   try {
-    if(display.value==="Error"){
-        throw error;
+    if (display.value === "Error") {
+      throw error;
     }
     display.value = eval(display.value);
   } catch (error) {
